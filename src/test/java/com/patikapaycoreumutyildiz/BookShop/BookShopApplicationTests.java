250ee26646010c8f30364c205d6c7f2f0d1920b4
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2022-01-17 21:36:25 +0300

    Project template created

---
+++ src/test/java/com/patikapaycoreumutyildiz/BookShop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patikapaycoreumutyildiz.BookShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
