0e5b4d68752d632b49d6e1a93ff45a0aa3022e43
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2022-02-10 01:13:12 +0300

    Cart Book controller created with composite key impl

--- BookShop/src/main/java/com/patikapaycoreumutyildiz/BookShop/Business/concretes/CartBookServiceImpl.java
+++ BookShop/src/main/java/com/patikapaycoreumutyildiz/BookShop/Business/concretes/CartBookServiceImpl.java
@@ -1,8 +1,52 @@
 package com.patikapaycoreumutyildiz.BookShop.Business.concretes;
 
 import com.patikapaycoreumutyildiz.BookShop.Business.abstracts.CartBookService;
+import com.patikapaycoreumutyildiz.BookShop.DataAccess.CartBookRepository;
+import com.patikapaycoreumutyildiz.BookShop.Entities.Cart;
+import com.patikapaycoreumutyildiz.BookShop.Entities.CartBook;
+import com.patikapaycoreumutyildiz.BookShop.Entities.CartBookId;
+import com.patikapaycoreumutyildiz.BookShop.Exception.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartBookServiceImpl implements CartBookService {
+
+    @Autowired
+    private CartBookRepository cartBookRepository;
+
+    @Override
+    public List<CartBook> getAllCartBooks() {
+        return cartBookRepository.findAll();
+    }
+
+    @Override
+    public CartBook getCartBook(CartBookId cartBookId) {//dao ile düzelt
+        Optional<CartBook> cartBook = cartBookRepository.findById(cartBookId);
+        return cartBook.orElseThrow(() -> new NotFoundException("CartBook"));
+
+    }
+
+    @Override
+    public void addCartBook(CartBook cartBook) {
+        cartBookRepository.save(cartBook);
+    }
+
+    @Override
+    public CartBook updateCartBook(CartBook cartBook) {
+        return cartBookRepository.save(cartBook);
+    }
+
+    @Override
+    public boolean deleteCartBook(CartBookId cartBookId) {
+        Optional<CartBook> cartBook = Optional.of(getCartBook(cartBookId));
+        if(cartBook.isPresent()){
+            cartBookRepository.delete(cartBook.get());
+            return true;
+        }
+        return false;
+    }
 }
--- BookShop/src/main/java/com/patikapaycoreumutyildiz/BookShop/DataAccess/CartBookRepository.java
+++ BookShop/src/main/java/com/patikapaycoreumutyildiz/BookShop/DataAccess/CartBookRepository.java
@@ -1,10 +1,17 @@
 package com.patikapaycoreumutyildiz.BookShop.DataAccess;
 
+import com.patikapaycoreumutyildiz.BookShop.Entities.Book;
+import com.patikapaycoreumutyildiz.BookShop.Entities.Cart;
 import com.patikapaycoreumutyildiz.BookShop.Entities.CartBook;
 import com.patikapaycoreumutyildiz.BookShop.Entities.CartBookId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartBookRepository extends JpaRepository<CartBook,CartBookId> {
+    List<CartBook> findCartBookByCartBookId_Cart(Cart cart);
+    List<CartBook> findCartBookByCartBookId_Book(Book book);
+
 }
