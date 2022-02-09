10a77fcb6c8050a4f0b2301128b65cf29a76973d
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2022-02-09 14:03:37 +0300

    Cart crud operations are implemented

--- BookShop/src/main/java/com/patikapaycoreumutyildiz/BookShop/Controller/CartController.java
+++ BookShop/src/main/java/com/patikapaycoreumutyildiz/BookShop/Controller/CartController.java
@@ -1,9 +1,13 @@
 package com.patikapaycoreumutyildiz.BookShop.Controller;
 
 import com.patikapaycoreumutyildiz.BookShop.Business.abstracts.CartService;
+import com.patikapaycoreumutyildiz.BookShop.Entities.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart/")
@@ -11,4 +15,29 @@ public class CartController {
 
     @Autowired
     private CartService cartService;
+
+    @GetMapping("all")
+    public List<Cart> getAllCarts() {
+        return cartService.getAllCarts();
+    }
+
+    @GetMapping("{id}")
+    public Cart getCart(@PathVariable @Min(1) Integer id) {
+        return cartService.getCart(id);
+    }
+
+    @PostMapping("create")
+    public void addCart(@Valid @RequestBody Cart cart) {
+        cartService.addCart(cart);
+    }
+
+    @PutMapping("update")
+    public Cart updateCart(@Valid @RequestBody Cart cart) {
+        return cartService.updateCart(cart);
+    }
+
+    @DeleteMapping("delete")
+    public boolean deleteCart(@RequestParam @Min(1) Integer id) {
+        return cartService.deleteCart(id);
+    }
 }
